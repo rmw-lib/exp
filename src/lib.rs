efd7e3ad98e55a419d8478e635b07ca4a8b828d6
gcxfd <i@rmw.link>
2022-07-12 12:42:08 +0800

    🔶

--- src/lib.rs
+++ src/lib.rs
@@ -2,29 +2,29 @@ pub use log::error;
 
 #[macro_errort]
 macro_rules! out {
-    ($err:errr) => {
-        err::error!("{}", $err);
-    };
+  ($err:expr) => {
+    err::error!("{}", $err);
+  };
 }
 
 #[macro_errort]
 macro_rules! ok {
-    ($result:errr) => {{
-        match $result {
-            Err(err) => {
-                err::out!(err);
-                Err(err)
-            }
-            Ok(val) => Ok(val),
-        }
-    }};
+  ($result:expr) => {{
+    match $result {
+      Err(err) => {
+        err::out!(err);
+        Err(err)
+      }
+      Ok(val) => Ok(val),
+    }
+  }};
 }
 
 #[macro_errort]
 macro_rules! log {
-    ($result:errr) => {{
-        if let Err(err) = $result {
-            err::out!(err);
-        }
-    }};
+  ($result:expr) => {{
+    if let Err(err) = $result {
+      err::out!(err);
+    }
+  }};
 }
